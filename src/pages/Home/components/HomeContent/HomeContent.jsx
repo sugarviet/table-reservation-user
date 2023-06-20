6026d6505eef8b100741cd1c500323b1f1809e50
K <thekhoi.k@gmail.com>
2023-06-20 12:51:37 +0700

    fix booking

--- src/pages/Home/components/HomeContent/HomeContent.jsx
+++ src/pages/Home/components/HomeContent/HomeContent.jsx
@@ -1,9 +1,9 @@
-import styles from './HomeContent.module.css'
-import { Select, Form, DatePicker, Space, Button } from 'antd';
-import Search from '../../../../assets/search.png'
-import HomeTable from '../HomeTable/HomeTable';
+import styles from "./HomeContent.module.css";
+import { Select, Form, DatePicker, Space, Button } from "antd";
+import Search from "../../../../assets/search.png";
+import HomeTable from "../HomeTable/HomeTable";
 import { onFinish } from "../../components/hooks/useSearchTable";
-import { useState } from 'react';
+import { useState } from "react";
 import axios from "axios";
 
 const HomeContent = () => {
@@ -11,15 +11,25 @@ const HomeContent = () => {
   const [data, setData] = useState();
   const [capacity, setCapacity] = useState(0);
   const [timeRangeType, setTimeRangeType] = useState("");
-  const API_URL = "http://localhost:7070";
+  // const API_URL = "http://localhost:7070";
 
-  const api = axios.create({
-    baseURL: API_URL,
-    withCredentials: true,
-  });
+  // const api = axios.create({
+  //   baseURL: API_URL,
+  //   withCredentials: true,
+  // });
   const handleFormSubmit = async () => {
     try {
-      const response = await api.get("/table/search", { params: { capacity: Number(capacity), timeRangeType } });
+      let api = `http://localhost:7070/table/search?`;
+      if (capacity !== 0) {
+        api += `capacity=${capacity}&`;
+      }
+
+      if (timeRangeType !== "") {
+        api += `timeRangeType=${timeRangeType}`;
+      }
+
+      const response = await axios.get(api);
+
       setData(response.data);
       console.log(data);
     } catch (error) {
@@ -31,10 +41,7 @@ const HomeContent = () => {
       <h1 className={styles.homeHeader}>Table Reservation Online</h1>
       <p className={styles.homeDes}>Book your table early at our restaurant</p>
       <div className={styles.homeSearch}>
-        <Form
-          className={styles.homeForm}
-          onFinish={handleFormSubmit}
-        >
+        <Form className={styles.homeForm} onFinish={handleFormSubmit}>
           <div className={styles.homeGroup}>
             <div className={styles.homeChoose}>
               <p className={styles.homeTitleChoose}>Capacity of table</p>
@@ -48,20 +55,20 @@ const HomeContent = () => {
                     bordered={false}
                     options={[
                       {
-                        value: '4',
-                        label: '4 people',
+                        value: "4",
+                        label: "4 people",
                       },
                       {
-                        value: '6',
-                        label: '6 people',
+                        value: "6",
+                        label: "6 people",
                       },
                       {
-                        value: '8',
-                        label: '8 people',
+                        value: "8",
+                        label: "8 people",
                       },
                       {
-                        value: '10',
-                        label: '10 people',
+                        value: "10",
+                        label: "10 people",
                       },
                     ]}
                     onChange={(value) => setCapacity(value)}
@@ -88,23 +95,23 @@ const HomeContent = () => {
                     options={[
                       {
                         value: "6h",
-                        label: '6:00',
+                        label: "6:00",
                       },
                       {
-                        value: '8h',
-                        label: '8:00',
+                        value: "8h",
+                        label: "8:00",
                       },
                       {
-                        value: '9h',
-                        label: '9:00',
+                        value: "9h",
+                        label: "9:00",
                       },
                       {
-                        value: '10h',
-                        label: '10:00',
+                        value: "10h",
+                        label: "10:00",
                       },
                       {
-                        value: '12h',
-                        label: '12:00',
+                        value: "12h",
+                        label: "12:00",
                       },
                     ]}
                     onChange={(value) => setTimeRangeType(value)}
@@ -116,11 +123,12 @@ const HomeContent = () => {
                     ))} */}
                   </Select>
                 </Space>
-              </Form.Item></div>
+              </Form.Item>
+            </div>
 
             <Button className={styles.homeBtn} htmlType="submit">
               <div className={styles.homeChoose1}>
-                <img style={{ width: '30px' }} src={Search}></img>
+                <img style={{ width: "30px" }} src={Search}></img>
                 <p className={styles.homeSearchTxt}>Search</p>
               </div>
             </Button>
@@ -129,12 +137,15 @@ const HomeContent = () => {
       </div>
       <div className={styles.homeInfor}>
         <h1>Find table for you</h1>
-        <p style={{ fontSize: '20px', marginTop: '10px' }}>Given your specific preferences, we will make recommendations to suit your requirements</p>
-        <h1 style={{ marginTop: '10px' }}> Or</h1>
-        <h1 style={{ marginTop: '10px' }}>Call us : 098123320</h1>
+        <p style={{ fontSize: "20px", marginTop: "10px" }}>
+          Given your specific preferences, we will make recommendations to suit
+          your requirements
+        </p>
+        <h1 style={{ marginTop: "10px" }}> Or</h1>
+        <h1 style={{ marginTop: "10px" }}>Call us : 098123320</h1>
       </div>
       <HomeTable data={data} />
     </div>
-  )
-}
+  );
+};
 export default HomeContent;
